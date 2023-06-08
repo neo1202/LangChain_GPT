0f687fe7e6f553385a3e9cdbe9342c713272368c
kevin <kevin@kevindeMacBook-Air.local>
2023-06-08 15:47:32 +0800

    kevin add Lang

--- flask-server/app.py
+++ flask-server/app.py
@@ -2,15 +2,23 @@ import re
 from datetime import datetime
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from config import API_KEY, DEBUG
+from config import OPEN_API_KEY, PINECONE_KEY, SERP_API_KEY
+from demoo import initialize, process_and_store_documents, get_my_agent
+from werkzeug.utils import secure_filename
+import os
+import tempfile
 
 app = Flask(__name__)
 CORS(app)
 # Access the config variables
-app.config['API_KEY'] = API_KEY
-app.config['DEBUG'] = DEBUG
+app.config['OPEN_API_KEY'] = OPEN_API_KEY
+app.config['SERP_API_KEY'] = SERP_API_KEY
+app.config['PINECONE_KEY'] = PINECONE_KEY
 
 #https://code.visualstudio.com/docs/python/tutorial-flask#_use-a-template-to-render-a-page
+
+llm_chat, embeddings = initialize()
+my_agent = get_my_agent()
 
 @app.route('/data')
 def get_time():
@@ -26,9 +34,10 @@ def process_input():
     data = request.get_json()
     print(f'receive data from User input: {data}')
     user_input = data.get('inputText') #等同於 data['inputText']
-    Ai_response =  '我操操操'+user_input
+    Ai_response =  my_agent.run(user_input)
     
     return jsonify({'response': Ai_response})
+
 #python -m pip install flask 
 
 #python -m venv .venv 
