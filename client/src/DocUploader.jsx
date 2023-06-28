f69f6d39b68bdad1de716495c838ada07901baab
neo1202 <neowuhuayo@gmail.com>
2023-06-28 15:49:38 +0800

    稍微正常版本

--- client/src/DocUploader.jsx
+++ client/src/DocUploader.jsx
@@ -5,24 +5,25 @@ const DocUploader = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    axios.post('http://127.0.0.1:5000/upload_folder', formData)
+    axios.post('http://127.0.0.1:5000/upload_doc', formData)
     .then((response) => {
         console.log(response.data);
     })
     .catch((error) => {
         console.error(error);
-        // Handle the error here
     });
   };
 
   return (
     <section className="absolute">
-        <form id="upload-form" encType="multipart/form-data" onSubmit={handleSubmit}>
+        <form id="upload-folder" encType="multipart/form-data" onSubmit={handleSubmit}>
+            <label htmlFor="file-input" className="display: inline-block mr-2 w-[120px]">Select Folder:</label>
             <input type="file" webkitdirectory="true" id="file-input" name="file" directory="" multiple/>
             <button type="submit">Upload</button>
         </form>
 
-        <form id="upload-form" encType="multipart/form-data" onSubmit={handleSubmit}>
+        <form id="upload-file" encType="multipart/form-data" onSubmit={handleSubmit}>
+            <label htmlFor="file-input" className="display: inline-block mr-2 w-[120px]">Select Files:</label>
             <input type="file" id="file-input" name="file" multiple/>
             <button type="submit">Upload</button>
         </form>
