a6c60104caacff888126e8bc4d07cc837b55e539
neo1202 <neowuhuayo@gmail.com>
2023-06-29 08:33:18 +0800

    0628 last modify

--- client/src/DocUploader.jsx
+++ client/src/DocUploader.jsx
@@ -8,6 +8,7 @@ const DocUploader = () => {
     axios.post('http://127.0.0.1:5000/upload_doc', formData)
     .then((response) => {
         console.log(response.data);
+        alert('上傳成功！')
     })
     .catch((error) => {
         console.error(error);
@@ -17,8 +18,8 @@ const DocUploader = () => {
   return (
     <section className="absolute">
         <form id="upload-folder" encType="multipart/form-data" onSubmit={handleSubmit}>
-            <label htmlFor="file-input" className="display: inline-block mr-2 w-[120px]">Select Folder:</label>
-            <input type="file" webkitdirectory="true" id="file-input" name="file" directory="" multiple/>
+            <label htmlFor="folder-input" className="display: inline-block mr-2 w-[120px]">Select Folder:</label>
+            <input type="file" webkitdirectory="true" id="folder-input" name="file" directory="" multiple/>
             <button type="submit">Upload</button>
         </form>
 
